be091b4168f70969b33643edf383d7f9d57f24df
Martez Reed <martez.reed@greenreedtech.com>
2024-05-28 14:57:17 -0500

    Update automation and workflow

--- trigger-workflow.py
+++ trigger-workflow.py
@@ -10,23 +10,14 @@ def trigger_workflow(workflow, ref, data):
     id = workflow.get_runs()[0].id
     print(workflow.get_runs()[0].status)
     return id
-    #for run in workflow.get_runs():
-    #    print(run.id)
-    #    print(run.run_started_at)
-    #    print(run.status)
-    #    print(run.event)
-        # Wait for the catalog item details to populate
-    #    time.sleep(15)
 
 def poll_workflow_execution(workflow, id):
     status = "in_progress"
-
     while status == "in_progress" or status == "queued":
         for run in workflow.get_runs():
             if run.id == id:
                 status = run.status
                 print("current status: " + status )
-                print("logs: " + run.logs_url)
                 code, headers, data = workflow._requester.requestJson("GET", run.logs_url)
                 print("code: " + str(code))
                 print(headers)
@@ -50,10 +41,9 @@ def main():
     workflow = repo.get_workflow(workflow_name)
 
     data = {}
-    data['print_tags'] = True
-    data['tags'] = "test"
+    data['message'] = morpheus['customOptions']['ghMessage']
 
-    ref = repo.get_branch("main")
+    ref = repo.get_branch(morpheus['customOptions']['gitBranch'])
     id = trigger_workflow(workflow, ref, data)
     poll_workflow_execution(workflow, id)
 
